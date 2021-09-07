a9aef9e4401c2979c8e90f865c1af8fd26da7ad4
Muhammad <77672824+muhammad23012009@users.noreply.github.com>
2021-09-07 13:34:10 +0500

    dandelion: Adapt for Redmi 9C

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,13 +17,13 @@
 # Inherit from xiaomi mt6765-common
 -include device/xiaomi/mt6765-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/xiaomi/cactus
+DEVICE_PATH := device/xiaomi/dandelion
 
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Kernel
-TARGET_KERNEL_CONFIG := cactus_defconfig
+TARGET_KERNEL_CONFIG := dandelion_defconfig
 
 # OTA
-TARGET_OTA_ASSERT_DEVICE := cactus
+TARGET_OTA_ASSERT_DEVICE := dandelion
